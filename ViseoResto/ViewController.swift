e9dc6172ab5d6c74851a81e25500c4d9586daa75
David Siscar <david.siscar_ssii@i-bp.fr>
2017-02-27 15:36:45 +0100

    add project

---
+++ ViseoResto/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ViseoResto
+//
+//  Created by SISCAR David (i-BP - CONSULTIME) on 27/02/2017.
+//  Copyright © 2017 Viseo. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("test")
+        
+    }
+    
+    
+    
+}
